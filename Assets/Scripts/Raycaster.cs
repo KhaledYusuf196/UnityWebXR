e551f43d2243085a8b930490591a99110d6ff012
Khaled Yousef <khaledyousef196@gmail.com>
2020-04-11 11:19:50 +0200

    First Commit

---
+++ Assets/Scripts/Raycaster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebXR;
+
+public class Raycaster : MonoBehaviour
+{
+    Transform currentCamera;
+    [SerializeField] Transform MainCamera, VRCamera;
+    [SerializeField] GameObject tartgetObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(WebXRManager.Instance.xrState == WebXRState.ENABLED)
+        {
+            currentCamera = VRCamera;
+        }
+        else
+        {
+            currentCamera = MainCamera;
+        }
+        if (Physics.Raycast(currentCamera.position,currentCamera.forward))
+        {
+
+        }
+    }
+}
